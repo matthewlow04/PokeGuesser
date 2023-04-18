54887133812c113b3e240f1c5c868cf2a757b971
Matthew <matthew7419@gmail.com>
2023-04-18 12:22:34 -0400

    Initial Commit

---
+++ PokeGuesser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeGuesser
+//
+//  Created by Matthew Low on 2023-04-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeGuesser/PokeGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeGuesserApp.swift
+//  PokeGuesser
+//
+//  Created by Matthew Low on 2023-04-18.
+//
+
+import SwiftUI
+
+@main
+struct PokeGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
